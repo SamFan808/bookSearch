b89ce6ea550401231d36206631c6280d0072aacb
Sam Fan <samisfan@msn.com>
2021-05-31 11:23:51 -0600

    updating API

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
